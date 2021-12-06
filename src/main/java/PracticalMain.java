1c827f96e06e1f0ed4a4e739c26d630b84156bcf
rvs16 <solovev.r@phystech.edu>
2021-12-06 03:27:18 +0300

    edited main class

--- src/main/java/PracticalMain.java
+++ src/main/java/PracticalMain.java
@@ -1,5 +1,7 @@
 public class PracticalMain {
     public static void main(String[] args) {
-
+        BigNumber a = new BigNumber("16020");
+        BigNumber b = new BigNumber("97000");
+        System.out.println(new BigNumberOperator().subtract(b, a));
     }
 }
